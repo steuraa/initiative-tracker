e3dfdaf3fa102681d9385ba489cbc1d2c4201bfb
steuraa <aaron.steurbaut@geosolutions.be>
2018-05-13 20:57:05 +0200

    start reactive central panel

--- src/app/tracker-module/components/central-field/central-field.component.ts
+++ src/app/tracker-module/components/central-field/central-field.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Encounter } from '../../../shared-module/models/encounter';
 import { EncounterDomainService } from '../../../shared-module/services/encounter-service/encounter-domain.service';
 import { StoreService } from '../../../shared-module/services/store-service/store.service';
@@ -11,11 +12,18 @@ import { StoreService } from '../../../shared-module/services/store-service/stor
 export class CentralFieldComponent {
   encounter: Encounter;
   tempEncounter: any;
+  encounterForm: FormGroup;
 
-  constructor(private encounterService: EncounterDomainService, private storeService: StoreService) {
+  constructor(private fb: FormBuilder, private encounterService: EncounterDomainService, private storeService: StoreService) {
+    this.encounterForm = this.fb.group({
+      name: ['', Validators.required],
+      heroes: this.fb.array([]),
+      monsters: this.fb.array([])
+    });
     this.storeService.encounterSubject.subscribe((enc: Encounter) => {
       this.encounter = enc;
       this.tempEncounter = Object.assign({}, this.encounter);
+      this.populateForm();
     });
     this.storeService.startEncounterSubject.subscribe(() => {
       this.tempEncounter = new Encounter();
@@ -37,6 +45,19 @@ export class CentralFieldComponent {
 
   addMonster(monster) {
     this.tempEncounter.monsters.push(monster);
+  }
+
+  populateForm() {
+    this.encounterForm.setValue({
+      'id': this.tempEncounter.id,
+      'name': this.tempEncounter.name,
+    });
+    const heroesFG = this.tempEncounter.heroes.map(h => this.fb.group(h));
+    const monstersFG = this.tempEncounter.monsters.map(m => this.fb.group(m));
+    const heroesArray = this.fb.array((heroesFG));
+    const monstersArray = this.fb.array((monstersFG));
+    this.encounterForm.setControl('heroes', heroesArray);
+    this.encounterForm.setControl('monsters', monstersArray);
   }
 
   removeHero(index: number) {

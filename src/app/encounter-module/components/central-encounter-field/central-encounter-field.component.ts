1ee363403be94b18aaf6b5702e4fafa29e1c8890
steuraa <aaron.steurbaut@geosolutions.be>
2018-05-20 10:48:59 +0200

    Hp changes flow through to encounter

--- src/app/encounter-module/components/central-encounter-field/central-encounter-field.component.ts
+++ src/app/encounter-module/components/central-encounter-field/central-encounter-field.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { EncounterHero } from '../../../shared-module/models/hero';
+import { EncounterMonster } from '../../../shared-module/models/monster';
 import { ProgressEncounter } from '../../../shared-module/models/progressEncounter';
 import { EncounterDomainService } from '../../../shared-module/services/encounter-service/encounter-domain.service';
 import { StoreService } from '../../../shared-module/services/store-service/store.service';
@@ -12,17 +14,16 @@ import { StoreService } from '../../../shared-module/services/store-service/stor
 export class CentralEncounterFieldComponent implements OnInit {
   currentIndex = 0;
   encounter: ProgressEncounter;
-  participants = [];
+  participants = Array<(EncounterHero | EncounterMonster)>();
   selected: number;
   showInitiative = false;
-  tempHeroes = Array<any>();
-  tempMonsters = Array<any>();
+  tempHeroes = Array<EncounterHero>();
+  tempMonsters = Array<EncounterMonster>();
 
 
   constructor(private encounterService: EncounterDomainService, private storeService: StoreService, private route: ActivatedRoute) {
     this.route.data.subscribe((data: { encounter: any }) => {
       this.encounter = new ProgressEncounter(data.encounter.data);
-      console.log('central::encounter::', this.encounter);
     });
   }
 
@@ -30,39 +31,64 @@ export class CentralEncounterFieldComponent implements OnInit {
     if (this.encounter.round === 1 && this.encounter.heroes[0].initiative === 0) {
       this.rollForInitiative();
     } else {
-      // this.handleParticipantsOrder();
+      this.handleParticipantsOrder(this.encounter.heroes, this.encounter.monsters);
     }
   }
 
-  calculateInitiative(init_mod) {
+  calculateInitiative(init_mod: number) {
     return init_mod + Math.floor(Math.random() * 20);
   }
 
-  handleParticipantsOrder(heroes, monsters?) {
+  changeHp(hp: string, index: number) {
+    if (hp) {
+      this.selected = undefined;
+      const cP = this.participants[index];
+      const mHp = cP.max_hp;
+      const cHp = cP.hp + parseInt(hp, 10);
+      cP.hp = (cHp > mHp) ? mHp : (cHp < 0) ? 0 : cHp;
+      this.participants[index].hp = cP.hp;
+      this.handleHpChange(cP);
+    }
+  }
+
+  handleHpChange(cP: (EncounterHero | EncounterMonster)) {
+    const cA = (cP.type === 'hero') ? this.encounter.heroes : this.encounter.monsters;
+    cA.findIndex((p: (EncounterHero | EncounterMonster)) => {
+      if (p._id === cP._id) {
+        p.hp = cP.hp;
+      }
+      return p._id === cP._id;
+    });
+    console.log(this.encounter);
+  }
+
+  handleParticipantsOrder(heroes: any, monsters?: Array<EncounterMonster>) {
     if (!monsters) {
       this.showInitiative = false;
       this.tempMonsters = heroes.monsters;
       this.tempHeroes = heroes.heroes;
-      this.participants = [...this.tempHeroes, ...this.tempMonsters];
-      this.participants.sort(function(a, b) {
-        if (a.initiative > b.initiative) {
+    } else {
+      this.tempHeroes = heroes;
+      this.tempMonsters = monsters;
+    }
+    this.participants = [...this.tempHeroes, ...this.tempMonsters];
+    this.participants.sort(function (a, b) {
+      if (a.initiative > b.initiative) {
+        return -1;
+      }
+      if (a.initiative < b.initiative) {
+        return 1;
+      }
+      if (a.initiative === b.initiative) {
+        if (a.init_mod > b.init_mod) {
           return -1;
         }
-        if (a.initiative < b.initiative) {
+        if (a.init_mod < b.init_mod) {
           return 1;
         }
-        if (a.initiative === b.initiative) {
-          if (a.init_mod > b.init_mod) {
-            return -1;
-          }
-          if (a.init_mod < b.init_mod) {
-            return 1;
-          }
-          return 0;
-        }
-      });
-    }
-    console.log(this.participants);
+        return 0;
+      }
+    });
   }
 
   rollForInitiative() {
